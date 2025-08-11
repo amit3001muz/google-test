73c645e80ac7d84d22263b8781d00fa0db9902b5
amit <amit3001muz@gmail.com>
2025-08-11 08:56:08 +0530

    new file added

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.31.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "b02b567a-e4cb-4c73-975e-4664c82c8fc3"
+  # Configuration options
+}
+resource "azurerm_resource_group" "gulal2rg" {
+  name     = "gulal2rg"
+  location = "west Europe"
+}
+resource "azurerm_storage_account" "gulal2stg" {
+  name                     = "gulal2stg"
+  resource_group_name      = azurerm_resource_group.gulal2rg.name
+  location                 = azurerm_resource_group.gulal2rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
