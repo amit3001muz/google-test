a810042bbbdb13876cec159c7ef7736ac757d6f5
amit <amit3001muz@gmail.com>
2025-08-11 09:32:20 +0530

    aaj phir kuchh add kiya hai

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 # ek din ek codder me kuchh kharcha pani ke liye code me usne kucjhh kiya.check   
 # iss liye hamhe ek kuchh kam kiya jo kafi dino baad se practice kiya hai 
 # ab hum yahi karenge.check
-  
+  # ab ek aur kahani add karenge 
+  # aaj to main git sikh kar rahunga 
+  # aur PR request or pull requst ke through karenge 
+  # yahi dekhna hai ab maja aa raha hai
+  #  good luck for your new project
 
 terraform {
   required_providers {
